31874ba83feb96fa0da16142b24857b21d5e9841
Vijayyadav2006 <vy945716@gmail.com>
2025-02-05 09:24:43 +0530

    typing

--- server/index.js
+++ server/index.js
@@ -1,69 +1,83 @@
 const express = require("express");
 const app = express();
-const http = require('http');
-const { Server } = require('socket.io');
+const http = require("http");
+const { Server } = require("socket.io");
 
 const server = http.createServer(app);
 const io = new Server(server);
-const userSocketMap ={};
+const userSocketMap = {};
+const userRoomMap = {}; // ✅ Track user's room
 
+// Get current date and time in a readable format
+const getCurrentTimestamp = () => {
+  return new Date().toLocaleString();
+};
 
-//alerrt new user joined
-const getAllConnectedClients=(roomId) => {
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-        (socketId) => {
-            return {
-                socketId,
-                username: userSocketMap[socketId],
-            };
-        }
-    ) //map
-}
+// Get all connected clients in a room
+const getAllConnectedClients = (roomId) => {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => ({
+      socketId,
+      username: userSocketMap[socketId],
+    })
+  );
+};
 
 io.on("connection", (socket) => {
-    //console.log(`User connected: ${socket.id}`); // Using backticks
-   socket.on('join',({roomId,username})=>{
+  socket.on("join", ({ roomId, username }) => {
     userSocketMap[socket.id] = username;
-    socket.join(roomId);
-    const clients= getAllConnectedClients(roomId);
+    userRoomMap[socket.id] = roomId; // ✅ Store the room ID
 
-    //notify to all user that new user are joined 
-    clients.forEach(({socketId}) =>{
-       io.to(socketId).emit('joined',{
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+
+    console.log(`[${getCurrentTimestamp()}] User '${username}' joined room '${roomId}'`);
+
+    // Notify all users that a new user has joined
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit("joined", {
         clients,
         username,
-        socketId:socket.id,
-       })
-    })
-});
-
-socket.on('code-change',({roomId,code}) =>{
-    socket.in(roomId).emit('code-change',{code});
-});
-
-socket.on("sync-code",({socketId,code}) =>{
-io.to(socketId).emit('sync-code',{code});
- });
-
-socket.on('disconnecting',() =>{
-    const rooms = [...socket.rooms];
-    rooms.forEach((roomId) => {
-        socket.in(roomId).emit("left",{
-            socketId:socket.id,
-            username: userSocketMap[socket.Id],
-        });
+        socketId: socket.id,
+      });
     });
+  });
+
+  socket.on("code-change", ({ roomId, code, senderSocketId }) => {
+    socket.to(roomId).emit("code-change", { code, senderSocketId });
+  });
+
+  socket.on("sync-code", ({ socketId, code }) => {
+    io.to(socketId).emit("sync-code", { code });
+  });
+
+  // Improved Typing Indicator with Throttling
+  socket.on("typing", ({ roomId, username }) => {
+    socket.to(roomId).emit("userTyping", { username });
+
+    setTimeout(() => {
+      socket.to(roomId).emit("userTyping", { username: "" });
+    }, 1500);
+  });
+
+  socket.on("disconnect", () => {
+    const username = userSocketMap[socket.id];
+    const roomId = userRoomMap[socket.id]; // ✅ Get the stored room ID
+
+    if (roomId) {
+      console.log(`[${getCurrentTimestamp()}] User '${username}' left room '${roomId}'`);
+
+      socket.to(roomId).emit("left", {
+        socketId: socket.id,
+        username,
+      });
+    }
+
+    // Cleanup
     delete userSocketMap[socket.id];
-    socket.leave();
-})
+    delete userRoomMap[socket.id]; // ✅ Remove room tracking
+  });
 });
-
-
-
-
-
-
-
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server running"));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
